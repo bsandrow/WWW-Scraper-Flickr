32ec84dab32f3c0b7b91794a48ba5ec24274b6ee
Brandon Sandrowicz <bsandrow@gmail.com>
2009-12-05 20:57:01 -0800

    initial commit

---
+++ lib/WWW/Scraper/Flickr.pm
@@ -0,0 +1,39 @@
+package WWW::Scraper::Flickr;
+
+use strict;
+use warnings;
+
+use Cwd;
+use LWP::UserAgent;
+use Carp qw/
+    croak
+/;
+
+our $VERSION = 0.1;
+
+sub new {
+    my $class = shift;
+    my $args  = shift;
+    my $self  = {};
+
+    $self->{ua} = $args->{ua} ? $args->{ua} : _getUserAgent();
+
+    return bless($self,$class);
+}
+
+sub _getUserAgent {
+    my $ua = LWP::UserAgent->new();
+    $ua->timeout(10);
+    $ua->env_proxy;
+    return $ua;
+}
+
+sub fetch {
+    my $self = shift;
+    my $args = shift;
+
+    croak "No URL supplied!" unless $args->{url};
+    croak "No output directory supplied!" unless $args->{dir};
+
+    
+}
